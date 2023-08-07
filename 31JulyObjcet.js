8b36d70c1a25b99c5fad03cd9f657cff19945d6a
AyeshaDeshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2023-08-07 09:19:16 +0530

    add file

--- 31JulyObjcet.js
+++ 31JulyObjcet.js
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    <script>
       let obj = {
         name: "Wecode",
         age: 12,
@@ -137,6 +128,4 @@
         car.make = "Suzuki";
       }
       console.log(car.make); //output: "Suzuki"
-    </script>
-  </body>
-</html>
+
