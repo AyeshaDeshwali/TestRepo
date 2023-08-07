3c87dce874dc2ca2b4729b32a65d5f3b404648bd
AyeshaDeshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2023-08-07 09:21:31 +0530

    add file

--- Loop-Exercis.js
+++ Loop-Exercis.js
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    <script>
       //   1. Print 1 to 100
       //   for (let i = 1; i <= 100; i++) {
       //     console.log(i);
@@ -278,6 +269,3 @@
       //   i++;
       // } while (i <= 100);
       // console.log(sum);
-    </script>
-  </body>
-</html>
