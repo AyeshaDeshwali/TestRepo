2bbd3b7e79c0180fcb0f3084364f72320de54b33
AyeshaDeshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2023-08-10 23:47:11 +0530

    add ObjProperties.js

--- Tech Gun/ObjProperties.js
+++ Tech Gun/ObjProperties.js
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    <script>
       // Ojbect
       //   let Person = {
       //     firstName: "Ayesha",
@@ -135,6 +126,4 @@
       // };
       // Person.age = 19;
       // Person.greet();
-    </script>
-  </body>
-</html>
+  
