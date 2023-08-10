dd9c288f2223442222ab80f5c7af53b8f4f2cac9
AyeshaDeshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2023-08-10 23:48:00 +0530

    add array-Destructuring.js

--- Tech Gun/array-Destructuring.js
+++ Tech Gun/array-Destructuring.js
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    <script>
       //   let book = ["Hindi", 200, 50];
       //   console.log(book[1]);esika sortcut niche
 
@@ -52,6 +43,4 @@
       }
       let [oneBook, TowBokk, ThreeBokk] = book();
       console.log(oneBook);
-    </script>
-  </body>
-</html>
+  
