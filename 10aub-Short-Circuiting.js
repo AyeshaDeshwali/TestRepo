001ae25abe3a866bbd82ab9f831171d6ab6e6b73
AyeshaDeshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2023-08-10 23:40:10 +0530

    10aub-Short-Circuiting.js

--- 10aub-Short-Circuiting.js
+++ 10aub-Short-Circuiting.js
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    <script>
+
       //   let a = 10  30  20 < 30;
       //   console.log(a);
 
@@ -64,6 +56,3 @@
       //   console.log(obj?.address?.sub?.city);
 
       //   ..................................................................
-    </script>
-  </body>
-</html>
